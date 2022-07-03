bbe3b403d03333b9ee0bb5b7cdcb22087e325086
Lieutenant-K <kts4047@gmail.com>
2022-07-04 00:46:47 +0900

    Programmers DP Number 4 PS

--- Programmers/DP/Number 4/main.py
+++ Programmers/DP/Number 4/main.py
@@ -1,16 +1,10 @@
 def solution(money):
-    dp1 = [0] * len(money)
-    dp1[0] = money[0]
-    dp1[1] = max(money[0], money[1])
-
-    for i in range(2, len(money)-1): # 첫 집을 무조건 터는 경우
-        dp1[i] = max(dp1[i-1], money[i]+dp1[i-2])
-
-    dp2 = [0] * len(money)
-    dp2[0] = 0
-    dp2[1] = money[1]
-
-    for i in range(2, len(money)): # 마지막 집을 무조건 터는 경우
-        dp2[i] = max(dp2[i-1], money[i]+dp2[i-2])
-
-    return max(max(dp1), max(dp2)) # 두 경우 중 최대
+    answer = 0
+    n = len(money)
+    d1 = [money[0], max(money[0], money[1])] + [0]*(n-2)
+    d2 = [0, money[1]] + [0]*(n-2)
+    for i in range(2, n):
+        if i < n-1:
+            d1[i] = max(d1[i-2] + money[i], d1[i-1])
+        d2[i] = max(d2[i-2] + money[i], d2[i-1])
+    return max(max(d1), max(d2))
