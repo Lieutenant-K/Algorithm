3c68b4d1c67a1f8eb554dac6a6f59608784e8012
Liuetenant-K <kts4047@gmail.com>
2023-01-13 23:16:07 +0900

    완전탐색 3번 풀이 수정

--- Programmers/Algorithm Kit/Brute Force/Number 3/main.py
+++ Programmers/Algorithm Kit/Brute Force/Number 3/main.py
@@ -1,6 +1,3 @@
 def solution(brown, yellow):
-    for n in range(1, int(yellow**0.5)+1):
-        if yellow % n == 0:
-            w, h = yellow//n, n
-            if (w+2)*2 + h*2 == brown:
-                return [w+2, h+2]
+    widths = [w for w in range(3, 2500) if 2*w**2 - w*(brown+4) + 2*(brown+yellow) == 0]
+    return [max(widths), min(widths)]
