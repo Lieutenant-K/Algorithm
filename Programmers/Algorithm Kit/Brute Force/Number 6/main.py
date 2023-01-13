863384e667eb904a3626a1d8fcfd713ac0dcb49b
Liuetenant-K <kts4047@gmail.com>
2023-01-14 00:46:16 +0900

    완전탐색 6번 풀이 수정

--- Programmers/Algorithm Kit/Brute Force/Number 6/main.py
+++ Programmers/Algorithm Kit/Brute Force/Number 6/main.py
@@ -1,14 +1,8 @@
-from itertools import permutations
 def solution(k, dungeons):
-    n = len(dungeons)
-    answer = -1
-    for case in list(permutations(list(range(n)))):
-        number, cnt = k, 0
-        for index in case:
-            if number < dungeons[index][0]:
-                break
-            number -= dungeons[index][1]
-            cnt += 1
-        answer = max(cnt, answer)
-
-    return answer
+    m = 0
+    for require, use in dungeons:
+        if k >= require:
+            new = dungeons[:]
+            new.remove([require, use])
+            m = max(m, 1+solution(k-use, new))
+    return m
